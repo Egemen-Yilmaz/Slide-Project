166c3352d3aca4900e045e84522ebc310dbd4af9
Losrias <egemenylmz01@gmail.com>
2023-03-19 18:07:18 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./data";
+function App() {
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+
+  const checkIndex = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return people.length - 1;
+    }
+    return number;
+  };
+
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkIndex(newIndex);
+    });
+  };
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return checkIndex(newIndex);
+    });
+  };
+
+  {
+    useEffect(() => {
+      let slider = setInterval(() => {
+        nextPerson();
+      }, 3000);
+      return () => clearInterval(slider);
+    }, [index]);
+  }
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>rewievs
+        </h2>
+      </div>
+      <div className="section-center">
+        {people.map((person, personIndex) => {
+          const { id, image, name, title, quote } = person;
+
+          let position = "nextSlide";
+          if (personIndex === index) {
+            position = "activeSlide";
+          }
+          if (
+            personIndex === index - 1 ||
+            (index === 0 && personIndex === people.length - 1)
+          ) {
+            position = "lastSlide";
+          }
+          return (
+            <article className={position} key={id}>
+              <img src={image} alt={name} className="person-img"></img>
+              <h5>{name}</h5>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon" />
+            </article>
+          );
+        })}
+        <button className="prev" onClick={prevPerson}>
+          <FiChevronLeft />
+        </button>
+        <button className="next" onClick={nextPerson}>
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,40 @@
+const people = [
+  {
+    id: 1,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
+    name: "maria ferguson",
+    title: "office manager",
+    quote:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    id: 2,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    name: "john doe",
+    title: "regular guy",
+    quote:
+      "Lectus mauris ultrices eros in cursus turpis massa tincidunt dui. Facilisi etiam dignissim diam quis enim lobortis. ",
+  },
+  {
+    id: 3,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959121/person-1_aufeoq.jpg",
+    name: "peter smith",
+    title: "product designer",
+    quote:
+      "Dui ut ornare lectus sit. In ornare quam viverra orci sagittis eu volutpat odio. Arcu cursus euismod quis viverra nibh cras pulvinar mattis nunc.",
+  },
+  {
+    id: 4,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    name: "susan andersen",
+    title: "the boss",
+    quote:
+      "Sit amet nisl purus in mollis nunc sed. Eget magna fermentum iaculis eu non diam. Volutpat consequat mauris nunc congue nisi vitae suscipit.",
+  },
+];
+
+export default people;
